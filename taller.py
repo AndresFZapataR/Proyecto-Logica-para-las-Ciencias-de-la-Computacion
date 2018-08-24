969d8f59ea17ceef4283eefd0d1a5b227cdc52c8
AndresFZapataR <38545379+AndresFZapataR@users.noreply.github.com>
2018-08-24 07:46:21 -0500

    Update taller.py

--- taller.py
+++ taller.py
@@ -3,22 +3,25 @@ class Tree(object):
     self.left = left
     self.right = right
     self.label = label
+
+conectivos_binarios = ['O','Y','>',"<>"]
 letras=["p","q","r"]
-interps=[]
-aux={}
-for a in letras:
-   aux[a]=1
-interps.append(aux)
-for a in letras
-   interps_aux=[i for i in inters]
-   for i in inters_aux:
-       aux1={}
-       for b in letras:
-           if a==b:
-                aux1[b]=1-i[b]
-           else:
-                aux1[b]=i[b]
-       interps.append(aux1)
-print "interpretaciones"
-for i in interps:
-   print i
+def genInterps():
+  interps=[]
+  aux={}
+  for a in letras:
+     aux[a]=1
+     interps.append(aux)
+  for a in letras:
+       interps_aux=[i for i in inters]
+       for i in inters_aux:
+          aux1={}
+          for b in letras:
+              if a==b:
+                  aux1[b]=1-i[b]
+              else:
+                  aux1[b]=i[b]
+      interps.append(aux1)
+  print "interpretaciones"
+  for i in interps:
+    print i
