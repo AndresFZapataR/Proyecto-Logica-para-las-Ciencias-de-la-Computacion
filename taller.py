f9c92762b6fa9c5390637be05f8513d2cf45dcab
santiagomartinezl <33555556+santiagomartinezl@users.noreply.github.com>
2018-08-24 07:33:00 -0500

    Update taller.py

--- taller.py
+++ taller.py
@@ -3,3 +3,22 @@ class Tree(object):
     self.left = left
     self.right = right
     self.label = label
+letras=["p","q","r"]
+interps=[]
+aux={}
+for a in letras:
+   aux[a]=1
+interps.append(aux)
+for a in letras
+   interps_aux=[i for i in inters]
+   for i in inters_aux:
+       aux1={}
+       for b in letras:
+           if a==b:
+                aux1[b]=1-i[b]
+           else:
+                aux1[b]=i[b]
+       interps.append(aux1)
+print "interpretaciones"
+for i in interps:
+   print i
