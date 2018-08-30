8a33c5b110cbc7e3265ee514de92a9527780b492
AndresFZapataR <38545379+AndresFZapataR@users.noreply.github.com>
2018-08-30 16:02:22 -0500

    Update taller.py

--- taller.py
+++ taller.py
@@ -5,38 +5,35 @@ class Tree(object):
     self.label = label
 
 conectivos_binarios = ['O','Y','>']
-letras=["p","q","r"]
+letras = ["p","q","r", "t", "s"]
 interps = []
 aux = {}
+#(-(p>q)>(rOs))Yt
 p=Tree('p',None,None)
 q=Tree('q',None,None)
 r=Tree('r',None,None)
-qor=Tree('O',q,r)
-poq=Tree('O',p,q)
-nop=Tree('-',None,p)
-noq=Tree('-',None,q)
-nor = Tree('-',None,r)
-pyq=Tree('Y',p,q)
-pyr=Tree('Y',p,r)
-a1=Tree('Y',p,qor)
-a2=Tree('O',pyq,pyr)
-c1=pyq
-c2=Tree('-',None,Tree('O',nop,noq))
-d1=Tree('>',p,q)
-d2=Tree('O',nop,q)
+t=Tree('t', None, None)
+s = Tree('s',None,None)
+pq = Tree('>',p,q)
+nopq = Tree('-', None, pq)
+ros = Tree('O',r,s)
+nopqros = Tree('>',nopq,ros)
+formula = Tree('Y', nopqros,t)
+
 for a in letras:
 	aux[a]=1
-     	interps.append(aux)
-  	for a in letras:
-       		interps_aux=[i for i in inters]
-       		for i in inters_aux:
-          		aux1={}
-          		for b in letras:
-              			if a==b:
-                  			aux1[b]=1-i[b]
-              			else:
-                  			aux1[b]=i[b]
-      			interps.append(aux1)
+interps.append(aux)
+for a in letras:
+       	interps_aux=[i for i in interps]
+       	for i in interps_aux:
+         	aux1={}
+          	for b in letras:
+              		if a==b:
+                  		aux1[b]=1-i[b]
+              		else:
+                  		aux1[b]=i[b]
+      		interps.append(aux1)
+
 print "interpretaciones"
 for i in interps:
     	print i
@@ -72,8 +69,12 @@ def equi(a,b,interps):
           		return False
 	return True
 
+intertodotrue = []
 
-print equi(a1,a2,interps)
-print equi(b1,b2,interps)
-print equi(c1,c2,interps)
-print equi(d1,d2,interps)
+for i in interps:
+	if val(formula, i):
+		intertodotrue.append(i)
+print "interpretaciones que hacen verdadera la formula: "
+for i in intertodotrue:
+	print i
+
